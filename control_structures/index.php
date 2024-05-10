c9cb34d4e57af8d22c1513a8c42825a3a326276e
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-05-10 17:06:11 +0300

    control_structures

--- control_structures/index.php
+++ control_structures/index.php
@@ -293,5 +293,378 @@ foreach ($arr as $key => $value) {
   if (0 === ($key % 2)) { // пропуск чётных чисел
     continue;
   }
-  echo $value . "\n";
+  echo $value . "\n";     // один три пять
 }
+
+echo '<br><br>';
+$i = 0;
+while ($i++ < 5) {
+  echo 'Внешний' . '<br>';
+  while (1) {
+    echo 'Средний' . '<br>';
+    while (1) {
+      echo 'Внутренний' . '<br>';
+      continue 3;
+    }
+    echo 'Это никогда не выведется.' . '<br>';
+  }
+  echo 'Это также не выведется.' . '<br>';
+}
+
+
+/*===SWITCH===*/
+
+echo '<hr>';
+
+$i = 2;
+
+// Оператор switch:
+switch ($i) {
+  case 0:
+    echo "i равно 0";
+    break;
+  case 1:
+    echo "i равно 1";
+    break;
+  case 2:
+    echo "i равно 2";
+    break;
+}
+
+echo '<br>';
+// Эквивалентно:
+if ($i == 0) {
+  echo "i равно 0";
+} elseif ($i == 1) {
+  echo "i равно 1";
+} elseif ($i == 2) {
+  echo "i равно 2";
+}
+
+echo '<br>';
+$i = 'шоколадка';
+switch ($i) {
+  case "яблоко":
+    echo "i это яблоко";
+    break;
+  case "шоколадка":
+    echo "i это шоколадка";
+    break;
+  case "пирог":
+    echo "i это пирог";
+    break;
+}
+
+/**
+Если не напишете оператор break в конце секции case, PHP будет продолжать исполнять команды следующей секции case
+**/
+
+echo '<br>';
+$i = 0;
+switch ($i) {
+  case 0:
+    echo "i равно 0";
+  case 1:
+    echo "i равно 1";
+  case 2:
+    echo "i равно 2";
+}
+
+echo '<br>';
+switch ($i) {
+  case 0:
+  case 1:
+  case 2:
+    echo "i меньше чем 3, но неотрицательный";
+    break;
+  case 3:
+    echo "i равно 3";
+}
+
+echo '<br>';
+$i = 4;
+switch ($i) {
+  case 0:
+    echo "i равно 0";
+    break;
+  case 1:
+    echo "i равно 1";
+    break;
+  case 2:
+    echo "i равно 2";
+    break;
+  default:
+    echo "i не равно 0, 1 или 2";
+}
+
+/**
+нельзя использовать для сложных оценок значения switch
+ **/
+
+echo '<br>';
+$target = 1;
+$start = 3;
+switch ($target) {
+  case $start - 1:
+    print "A";
+    break;
+  case $start - 2:
+    print "B";
+    break;
+  case $start - 3:
+    print "C";
+    break;
+  case $start - 4:
+    print "D";
+    break;
+}
+
+echo '<br>';
+$offset = 1;
+$start = 3;
+switch (true) {
+  case $start - $offset === 1:
+    print "A";
+    break;
+  case $start - $offset === 2:
+    print "B";
+    break;
+  case $start - $offset === 3:
+    print "C";
+    break;
+  case $start - $offset === 4:
+    print "D";
+    break;
+}
+
+/**
+Альтернативный синтаксис для управляющей структуры switch
+ */
+
+echo '<br>';
+$i = 4;
+switch ($i):
+  case 0:
+    echo "i равно 0";
+    break;
+  case 1:
+    echo "i равно 1";
+    break;
+  case 2:
+    echo "i равно 2";
+    break;
+  default:
+    echo "i не равно 0, 1 или 2";
+endswitch;
+
+echo '<br>';
+$beer = 4;
+switch($beer)
+{
+  case 'tuborg';
+  case 'carlsberg';
+  case 'stella';
+  case 'heineken';
+    echo 'Хороший выбор';
+    break;
+  default;
+    echo 'Пожалуйста, сделайте новый выбор...';
+    break;
+}
+
+
+/*===MATCH===*/
+
+/**
+ * Используется строгое сравнение (===), а не слабое (==).
+ * Выражение match доступно начиная с PHP 8.0.0.
+**/
+
+echo '<hr>';
+$food = 'cake';
+$return_value = match ($food) {
+  'apple' => 'На столе лежит яблоко',
+  'banana' => 'На столе лежит банан',
+  'cake' => 'На столе стоит торт',
+};
+var_dump($return_value);
+
+echo '<br>';
+//$result = match ($x) {
+//  // Множественное условие:
+//  $a, $b, $c => 5,
+//  // Аналогично трём одиночным:
+//  $a => 5,
+//  $b => 5,
+//  $c => 5,
+//};
+
+echo '<br>';
+//$expressionResult = match ($condition) {
+//  1, 2 => foo(),
+//  3, 4 => bar(),
+//  default => baz(),
+//};
+
+//Пример необработанного выражения
+echo '<br>';
+$condition = 5;
+try {
+  match ($condition) {
+    1, 2 => foo(),
+    3, 4 => bar(),
+  };
+} catch (\UnhandledMatchError $e) {
+  var_dump($e);
+}
+
+//  Использование match для ветвления в зависимости от вхождения в диапазоны целых чисел
+
+$age = 23;
+$result = match (true) {
+  $age >= 65 => 'пожилой',
+  $age >= 25 => 'взрослый',
+  $age >= 18 => 'совершеннолетний',
+  default => 'ребёнок',
+};
+var_dump($result);
+
+//  Использование match для ветвления в зависимости от содержимого строки
+
+$text2 = 'Bienvenue chez nous';
+
+$result2 = match (true) {
+  str_contains($text2, 'Welcome') || str_contains($text2, 'Hello') => 'en',
+  str_contains($text2, 'Bienvenue') || str_contains($text2, 'Bonjour') => 'fr',
+};
+var_dump($result2);
+
+
+/*===DECLARE===*/
+
+echo '<hr>';
+// Правильно:
+//declare(ticks=1);
+
+// Недопустимо:
+//const TICK_VALUE = 1;
+//declare(ticks=TICK_VALUE);
+
+// Пример использования тика
+declare(ticks=1);
+// Функция, исполняемая при каждом тике
+function tick_handler()
+{
+//  echo "Вызывается tick_handler()\n";
+}
+register_shutdown_function('tick_handler'); // вызывает событие тика
+$a = 1;           // вызывает событие тика
+if ($a > 0) {
+  $a += 2;        // вызывает событие тика
+  var_dump($a);   // вызывает событие тика
+}
+
+
+/*===RETURN===*/
+
+echo '<hr>';
+//https://www.php.net/manual/ru/functions.returning-values.php
+
+
+/*===REQUIRE===*/
+
+/**
+ * Выражение require остановит выполнение скрипта, тогда как выражение include только выдаёт предупреждение уровня E_WARNING, которое разрешает скрипту продолжить работу.
+**/
+
+
+/*===INCLUDE===*/
+
+echo '<hr>';
+echo "Одно $color $fruit"; // Одно
+echo '<br>';
+include 'test.php';
+echo "Одно $color $fruit"; // Одно зелёное яблоко
+echo '<br>';
+function foo()
+{
+  global $color2;
+  include 'test.php';
+  echo "Одно $color2 $fruit2";
+}
+foo();                     // Одно зелёное яблоко
+echo '<br>';
+echo "Одно $color2 $fruit2"; // Одно зелёное
+
+// Не сработает, PHP интерпретирует как include(('vars.php') == TRUE), то есть include('1')
+if (include('test.php') == TRUE) {
+  echo 'OK';
+}
+
+// Cработает
+if ((include 'test.php') == TRUE) {
+  echo 'OK';
+}
+
+echo '<br>';
+$foo = include 'return.php';
+echo $foo . '<br>';             // PHP
+$bar = include 'noreturn.php';
+echo $bar . '<br>';             // 1
+
+//  Буферизация вывода и включение файла PHP в строку
+
+$string = get_include_contents('somefile.php');
+function get_include_contents($filename) {
+  if (is_file($filename)) {
+    ob_start();
+    include $filename;
+    return ob_get_clean();
+  }
+  return false;
+}
+
+
+/*===REQUIRE_ONCE===*/
+
+/**
+ * Выражение require_once аналогично require за исключением того, что PHP проверит, включался ли уже данный файл, и если да, не будет включать его ещё раз.
+ */
+
+
+/*===INCLUDE_ONCE===*/
+
+/**
+ * include_once может использоваться в тех случаях, когда один и тот же файл может быть включён и выполнен более одного раза во время выполнения скрипта, в данном случае это поможет избежать проблем с переопределением функций, переменных и т.д.
+**/
+
+
+/*===GOTO===*/
+
+echo '<hr>';
+goto a;
+echo 'Foo';
+a:
+echo 'Bar';         // Bar
+
+//echo '<br>';
+//for ($i = 0, $j = 50; $i < 100; $i++) {
+//  while ($j--) {
+//    if ($i == 17) {
+//      goto end;
+//    }
+//  }
+//}
+//echo "i=$i";
+//end:
+//echo 'j hit 17';
+
+//Это не сработает
+//echo '<br>';
+//goto loop;
+//for ($i = 0, $j = 50; $i < 100; $i++) {
+//  while ($j--) {
+//    loop:
+//  }
+//}
+//echo "$i = $i";
